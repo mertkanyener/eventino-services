c7cc07c47a1c6846bb86bb5536ae4d4516ffa962
Mertkan Yener <mertkanyener@outlook.com>
2023-12-07 18:37:54 +0300

    first commit

---
+++ src/main/java/com/eventino/msartist/entity/Artist.java
@@ -0,0 +1,14 @@
+package com.eventino.msartist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Artist {
+}
---
+++ src/test/java/com/eventino/msartist/MsArtistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventino.msartist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsArtistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
